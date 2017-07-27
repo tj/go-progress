cb74f8fe61244459e38e25651dfc8313c677cced
TJ Holowaychuk <tj@vision-media.ca>
2017-07-27 12:28:58 -0700

    add example with centering / colors

---
+++ _example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/tj/go-progress"
+	"github.com/tj/go/ansi"
+
+	color "github.com/aybabtme/rgbterm"
+)
+
+// gray string.
+func gray(s string) string {
+	return color.FgString(s, 150, 150, 150)
+}
+
+// purple string.
+func purple(s string) string {
+	return color.FgString(s, 96, 97, 190)
+}
+
+func main() {
+	b := progress.NewInt(10)
+	b.Width = 25
+	b.StartDelimiter = gray("|")
+	b.EndDelimiter = gray("|")
+	b.Filled = purple("█")
+	b.Empty = gray("░")
+
+	for i := 0; i <= 10; i++ {
+		b.ValueInt(i)
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		os.Stdout.WriteString(ansi.CenterLine(b.String()))
+	}
+}
